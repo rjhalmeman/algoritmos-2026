d734f228775a3a4d4ae0f3fd73bf4b5a2e7ba0b9
Radames <rjhalmeman@gmail.com>
2026-04-27 08:03:45 -0300

    27/04/2026 - 08:03:45

--- 1-Bimestre/exercícios/2026-04-24 e 27-autoavaliativa/1-funcaoBissexto.js
+++ 1-Bimestre/exercícios/2026-04-24 e 27-autoavaliativa/1-funcaoBissexto.js
@@ -7,6 +7,15 @@ function ehBissextoSimples(ano) {
     }
 }
 
+function ehBissextoSimplesRespostaTextual(ano) {
+    // essa função verifica se um ano é bissexto, mas não leva em consideração as regras completas para anos bissextos
+    if (ano % 4 === 0) {
+        return "sim"; // O ano é bissexto
+    } else {
+        return "não"; // O ano não é bissexto
+    }
+}
+
 function ehBissextoMelhorado(ano) {
     //Anos divisíveis por 100 não são bissextos, a menos que sejam divisíveis por 400. Por isso, 2000 foi bissexto, mas 1900 não foi.
     if ((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)) {
