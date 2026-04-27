188dee4c0959b0f26df6aa32762f7b2678d69176
Radames <rjhalmeman@gmail.com>
2026-04-27 07:56:54 -0300

    27/04/2026 - 07:56:54

--- 1-Bimestre/exercícios/2026-04-24 e 27-autoavaliativa/1a-minhasFuncoes.js
+++ 1-Bimestre/exercícios/2026-04-24 e 27-autoavaliativa/1a-minhasFuncoes.js
@@ -1,3 +1,13 @@
+/*
+Essas funções são exemplos de como criar funções em JavaScript para realizar tarefas específicas, como verificar se um ano é bissexto, determinar se um número é par ou ímpar, e calcular raízes cúbicas e enésimas.
+
+Há uma biblioteca de funções que pode ser usada em diferentes partes do código para evitar repetição e tornar o código mais organizado e fácil de manter. Por exemplo, a função `ehBissextoMelhorado` pode ser usada sempre que for necessário verificar se um ano é bissexto, garantindo que as regras corretas sejam aplicadas.
+
+no programa que vai chamar essas funções, basta importar ou incluir este arquivo de funções e então chamar as funções conforme necessário, passando os argumentos apropriados. Isso torna o código mais modular e reutilizável.
+
+*/
+
+
 function ehBissextoSimples(ano) {
     // essa função verifica se um ano é bissexto, mas não leva em consideração as regras completas para anos bissextos
     if (ano % 4 === 0) {
