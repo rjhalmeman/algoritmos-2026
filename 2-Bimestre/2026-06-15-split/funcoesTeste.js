1bd4952c90ca7c3986b08d766f41b9bc256bcd9c
Radames <rjhalmeman@gmail.com>
2026-06-15 07:41:00 -0300

    15/06/2026 - 07:41:00

--- 2-Bimestre/2026-06-15-split/funcoesTeste.js
+++ 2-Bimestre/2026-06-15-split/funcoesTeste.js
@@ -3,17 +3,17 @@ function contarPalavras(frase) {
     return palavras.length;
 }
 
-function naoFunciona(palavra){
+function issoNaoFunciona(palavra){
     palavra[0] = palavra[0].toUpperCase();
     return palavra;
 }
 
-function funciona(palavra){
+function maneiraCorretaDeModificarStrings(palavra){
     let aux = palavra.split(''); //converte a string em um vetor de caracteres
     aux[0] = aux[0].toUpperCase();
     return aux.join(''); //converte o vetor de caracteres de volta para string
 }
 
 console.log(contarPalavras("a copa do mundo atrapalha a vida do professor"));
-console.log(naoFunciona("banana"));
-console.log(funciona("banana"));
+console.log(issoNaoFunciona("banana"));
+console.log(maneiraCorretaDeModificarStrings("banana"));
