8bfa8c57180ba5e818a27614910d4c363bd911d2
RADAMES JULIANO HALMEMAN <rjhalmeman@gmail.com>
2026-07-06 10:30:24 -0300

    ajustei o pushTo

--- pushTo.sh
+++ pushTo.sh
@@ -42,10 +42,3 @@ else
     commit_message="$timestamp - $*"
 fi
 
-# Realiza um commit com a mensagem
-git commit -m "$commit_message"
-
-# Faz o push para o repositório remoto
-git push "$remote_name" "$branch_name"
-
-git push
